1fc9777e504cdb92d366781aa24da246fdf838db
Acerao <66062811+Acerao@users.noreply.github.com>
2021-06-15 13:10:10 +0800

    Assets

---
+++ Assets/PlayerController.cs
@@ -0,0 +1,89 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float moveSpeed;
+    public float gravityModifier;
+    public float jumpPower;
+    public float runSpeed;
+    public CharacterController charCon;
+
+    private Vector3 moveInput;
+
+    public Transform camTrans;
+    public float mouseSensitivity;
+
+    private bool canJump;
+    private bool canDoubleJump;
+    public Transform groundCheckPoint;
+    public LayerMask WhatIsGround;
+
+    public Animator anim;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //moveInput.x = Input.GetAxis("Horizontal") * moveSpeed * Time.deltaTime;
+        //moveInput.z = Input.GetAxis("Vertical") * moveSpeed * Time.deltaTime;
+
+        //store y velocity
+        float yStore = moveInput.y;
+
+        Vector3 vertMove = transform.forward * Input.GetAxis("Vertical");
+        Vector3 horitMove = transform.right * Input.GetAxis("Horizontal");
+
+        moveInput = vertMove + horitMove;
+        moveInput.Normalize();
+
+        if (Input.GetKey(KeyCode.LeftShift))
+        {
+            moveInput *= runSpeed;
+        }
+        else
+        {
+            moveInput *= moveSpeed;
+        }
+        
+
+        moveInput.y = yStore;
+        moveInput.y += Physics.gravity.y * gravityModifier * Time.deltaTime;
+
+        if (charCon.isGrounded)
+        {
+            moveInput.y = Physics.gravity.y * gravityModifier * Time.deltaTime;
+        }
+
+        canJump = Physics.OverlapSphere(groundCheckPoint.position, 0.25f, WhatIsGround).Length > 0;
+
+        //Handle Jumping
+        if (Input.GetKeyDown(KeyCode.Space)&&canJump)
+        {
+            moveInput.y = jumpPower;
+            canDoubleJump = true;
+        }
+        else if(canDoubleJump && Input.GetKeyDown(KeyCode.Space))
+        {
+            moveInput.y = jumpPower;
+            canDoubleJump = false;
+        }
+
+        charCon.Move(moveInput * Time.deltaTime);
+
+        //control camera rotation
+        Vector2 mouseInput = new Vector2(Input.GetAxisRaw("Mouse X"), Input.GetAxisRaw("Mouse Y"))*mouseSensitivity;
+
+        //we rotate y
+        transform.rotation = Quaternion.Euler(transform.rotation.eulerAngles.x, transform.rotation.eulerAngles.y + mouseInput.x,transform.rotation.eulerAngles.z);
+
+        camTrans.rotation = Quaternion.Euler(camTrans.rotation.eulerAngles + new Vector3(-mouseInput.y, 0f, 0f));
+
+        anim.SetFloat("moveSpeed", moveInput.magnitude);
+    }
+}
